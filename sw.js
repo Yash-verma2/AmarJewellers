b57af51bef52dec2ca8c9898d0cc42275f12a461
Yash-verma2 <yash04523@gmail.com>
2025-10-12 18:06:00 +0530

    Add files via upload
    new folder

--- sw.js
+++ sw.js
@@ -1,5 +1,31 @@
-self.addEventListener('install', e => {
-  console.log('Service Worker installed');
+const CACHE_NAME = 'amar-jewellers-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Basic service worker: cache-on-install, serve-from-cache fallback to network
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
 });
 
-self.addEventListener('fetch', e => {});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        return response;
+      }).catch(() => {
+        // fallback could be added here
+      });
+    })
+  );
+});
